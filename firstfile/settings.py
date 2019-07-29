07f98a856c8a7cb24572fe9dfd67f88f17ce0399
Heatspurple <soonsoo564@gmail.com>
2019-07-30 00:18:20 +0900

    .

--- firstfile/settings.py
+++ firstfile/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'firstfile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'minsung',
-        'USER' : 'name',
-        'PASSWORD' : '',
-        'HOST' : 'localhost',
-        'PORT' : '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
